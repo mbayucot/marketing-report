60a54daa46c5311d502e4c2ef7b60b1395ad1058
Michael Bayucot <mbayucot@gmail.com>
2022-03-08 21:01:08 +0800

    Lint fixes

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -28,7 +28,7 @@ const DashboardPage = () => {
   return (
     <>
       <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-        <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">
+        <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#/">
           Marketing Report
         </a>
         <button
@@ -50,7 +50,7 @@ const DashboardPage = () => {
         />
         <div className="navbar-nav">
           <div className="nav-item text-nowrap">
-            <a className="nav-link px-3" href="#" onClick={handleSignout}>
+            <a className="nav-link px-3" href="#/" onClick={handleSignout}>
               Sign out
             </a>
           </div>
@@ -66,37 +66,37 @@ const DashboardPage = () => {
             <div className="position-sticky pt-3">
               <ul className="nav flex-column">
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">
+                  <a className="nav-link active" aria-current="page" href="#/">
                     <span data-feather="home"></span>
                     Dashboard
                   </a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">
+                  <a className="nav-link" href="#/">
                     <span data-feather="file"></span>
                     Orders
                   </a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">
+                  <a className="nav-link" href="#/">
                     <span data-feather="shopping-cart"></span>
                     Products
                   </a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">
+                  <a className="nav-link" href="#/">
                     <span data-feather="users"></span>
                     Customers
                   </a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">
+                  <a className="nav-link" href="#/">
                     <span data-feather="bar-chart-2"></span>
                     Reports
                   </a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">
+                  <a className="nav-link" href="#/">
                     <span data-feather="layers"></span>
                     Integrations
                   </a>
@@ -107,7 +107,7 @@ const DashboardPage = () => {
                 <span>Saved reports</span>
                 <a
                   className="link-secondary"
-                  href="#"
+                  href="#/"
                   aria-label="Add a new report"
                 >
                   <span data-feather="plus-circle"></span>
@@ -115,25 +115,25 @@ const DashboardPage = () => {
               </h6>
               <ul className="nav flex-column mb-2">
                 <li className="nav-item">
-                  <a className="nav-link" href="#">
+                  <a className="nav-link" href="#/">
                     <span data-feather="file-text"></span>
                     Current month
                   </a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">
+                  <a className="nav-link" href="#/">
                     <span data-feather="file-text"></span>
                     Last quarter
                   </a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">
+                  <a className="nav-link" href="#/">
                     <span data-feather="file-text"></span>
                     Social engagement
                   </a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">
+                  <a className="nav-link" href="#/">
                     <span data-feather="file-text"></span>
                     Year-end sale
                   </a>
