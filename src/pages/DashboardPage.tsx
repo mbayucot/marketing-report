3e497f53ae6a1d07e948da224f325814d9222674
Michael Bayucot <mbayucot@gmail.com>
2022-03-08 20:49:27 +0800

    Initial commit

---
+++ src/pages/DashboardPage.tsx
@@ -0,0 +1,208 @@
+import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+import { useParams, useHistory } from "react-router-dom";
+
+import "../Dashboard.css";
+import { clients } from "../constants";
+
+type Params = {
+  clientId: string;
+};
+
+const DashboardPage = () => {
+  let history = useHistory();
+  let { clientId } = useParams<Params>();
+
+  const found = clients.find((login) => login.id.toString() == clientId);
+
+  const [selected, setSelected] = useState(false);
+
+  const onSelectChange = (e: any) => {
+    setSelected(true);
+  };
+
+  const handleSignout = () => {
+    history.push(`/`);
+  };
+
+  return (
+    <>
+      <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+        <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">
+          Marketing Report
+        </a>
+        <button
+          className="navbar-toggler position-absolute d-md-none collapsed"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#sidebarMenu"
+          aria-controls="sidebarMenu"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <input
+          className="form-control form-control-dark w-100"
+          type="text"
+          placeholder="Search"
+          aria-label="Search"
+        />
+        <div className="navbar-nav">
+          <div className="nav-item text-nowrap">
+            <a className="nav-link px-3" href="#" onClick={handleSignout}>
+              Sign out
+            </a>
+          </div>
+        </div>
+      </header>
+
+      <div className="container-fluid">
+        <div className="row">
+          <nav
+            id="sidebarMenu"
+            className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse"
+          >
+            <div className="position-sticky pt-3">
+              <ul className="nav flex-column">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">
+                    <span data-feather="home"></span>
+                    Dashboard
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    <span data-feather="file"></span>
+                    Orders
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    <span data-feather="shopping-cart"></span>
+                    Products
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    <span data-feather="users"></span>
+                    Customers
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    <span data-feather="bar-chart-2"></span>
+                    Reports
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    <span data-feather="layers"></span>
+                    Integrations
+                  </a>
+                </li>
+              </ul>
+
+              <h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+                <span>Saved reports</span>
+                <a
+                  className="link-secondary"
+                  href="#"
+                  aria-label="Add a new report"
+                >
+                  <span data-feather="plus-circle"></span>
+                </a>
+              </h6>
+              <ul className="nav flex-column mb-2">
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    <span data-feather="file-text"></span>
+                    Current month
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    <span data-feather="file-text"></span>
+                    Last quarter
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    <span data-feather="file-text"></span>
+                    Social engagement
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    <span data-feather="file-text"></span>
+                    Year-end sale
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+
+          <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+            <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+              <h1 className="h2">Dashboard</h1>
+              <div className="btn-toolbar mb-2 mb-md-0">
+                <div className="btn-group me-2">
+                  <button
+                    type="button"
+                    className="btn btn-sm btn-outline-secondary"
+                  >
+                    Share
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-sm btn-outline-secondary"
+                  >
+                    Export
+                  </button>
+                </div>
+                <button
+                  type="button"
+                  className="btn btn-sm btn-outline-secondary dropdown-toggle"
+                >
+                  <span data-feather="calendar"></span>
+                  This week
+                </button>
+              </div>
+            </div>
+
+            <div style={{ height: "550px" }}>
+              <Form.Select
+                aria-label="Default select example"
+                onChange={onSelectChange}
+                defaultValue=""
+              >
+                <option value="" disabled>
+                  Select Account
+                </option>
+                {found &&
+                  found.name.map((client) => (
+                    <option key={client} value={client}>
+                      {client}
+                    </option>
+                  ))}
+              </Form.Select>
+              <br />
+              {selected && (
+                <iframe
+                  width="100%"
+                  height="100%"
+                  src="https://datastudio.google.com/embed/reporting/1ldFFV3R1IGJTGG8BwScQhNXD2u7H_4Bh/page/MwXb"
+                  frameBorder="0"
+                  style={{ border: "0" }}
+                  allowFullScreen
+                ></iframe>
+              )}
+            </div>
+          </main>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DashboardPage;
---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { Alert } from "react-bootstrap";
+import "../App.css";
+
+import { clients } from "../constants";
+
+const LoginPage = () => {
+  let history = useHistory();
+
+  const [error, setError] = useState(false);
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+
+    const found = clients.find(
+      (login) => login.email == email && login.password == password
+    );
+    if (found) {
+      history.push(`/dashboard/${found.id}`);
+      setError(false);
+    } else {
+      setError(true);
+    }
+  };
+
+  return (
+    <section className="form-signin">
+      <form onSubmit={handleSubmit}>
+        <img
+          className="mb-4"
+          src="https://getbootstrap.com/docs/5.1/assets/brand/bootstrap-logo.svg"
+          alt=""
+          width="72"
+          height="57"
+        />
+        <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+
+        {error && (
+          <Alert variant="success">Email and Password does not match</Alert>
+        )}
+
+        <div className="form-floating">
+          <input
+            type="email"
+            className="form-control"
+            id="floatingInput"
+            placeholder="name@example.com"
+            name="email"
+            required
+          />
+          <label htmlFor="floatingInput">Email address</label>
+        </div>
+        <div className="form-floating">
+          <input
+            type="password"
+            name="password"
+            className="form-control"
+            id="floatingPassword"
+            placeholder="Password"
+            required
+          />
+          <label htmlFor="floatingPassword">Password</label>
+        </div>
+
+        <div className="checkbox mb-3">
+          <label>
+            <input type="checkbox" value="remember-me" /> Remember me
+          </label>
+        </div>
+        <button className="w-100 btn btn-lg btn-primary" type="submit">
+          Sign in
+        </button>
+      </form>
+    </section>
+  );
+};
+
+export default LoginPage;
